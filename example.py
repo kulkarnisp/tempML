13658a3a9d59d4ad150835bdc1c34a7f115cdaa2
kulkarnisp <33883228+kulkarnisp@users.noreply.github.com>
2020-02-26 22:01:18 +0530

    Example run using abstract classes

--- example.py
+++ example.py
@@ -2,12 +2,20 @@ import pandas as pd
 import numpy as np
 
 import bayes
-import knn
-from commonfun import cal_loss, test_train_df
+import neighbors
+from commonfun import cal_loss, test_train_dt
 
 db3a = pd.read_csv("data\P1a_train_data.txt",header=None)
 db3b = pd.read_csv("data\P1b_train_data.txt",header=None)
 
+x_train,y_train = test_train_dt(db1a)
+x_test,y_test = test_train_dt(dt1a)
+
 
 clf = bayes.Bayes()
-clf.fit()
+clf.fit(x_train,y_train)
+print("result for q1 Bayes:", clf.params)
+
+clf = neighbors.Knn()
+clf.fit(x_train,y_train)
+print("result for q1 knn:", clf.params)
