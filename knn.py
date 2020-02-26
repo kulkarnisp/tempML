ef8de0e66a6888cad98a4c34c94cff5d3160f626
kulkarnisp <33883228+kulkarnisp@users.noreply.github.com>
2020-02-26 21:47:24 +0530

    Knn abstract class

---
+++ knn.py
@@ -0,0 +1,36 @@
+import random as rd
+import numpy as np
+from commonfun import euclidian
+
+class knn:
+    def __init__(self,k,verb=True):
+        self.k = k
+        self.verbose = verb
+    
+    def fit(self,x_train,y_train,dist=euclidian):
+        self.dist = euclidian
+        self.x_train = x_train
+        self.y_train = y_train
+        if self.verbose:
+            self.zloss()
+            print("training loss is:", self.loss)
+        
+    def test(self,x):
+        dist_arr = [self.dist(x,xd) for xd in self.x_train]
+        indices = np.argsort(dist_arr)
+        tot= sum(self.y_train[indices[:self.k]])
+        if tot!=0:
+            return tot/abs(tot)
+        else:
+            return rd.choice([1,-1])
+    
+    def predict(self,x_test):
+        y_pred = [self.test(x) for x in x_test]
+        return np.array(y_pred)
+        
+    def zloss(self):
+        self.loss = cal_loss(self.predict(self.x_train),
+                            self.y_train)
+        if self.verbose:
+            print("training loss is:", self.loss)
+    
